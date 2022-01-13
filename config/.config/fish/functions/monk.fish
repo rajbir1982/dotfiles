a2fc3f908e05d158ed0eed5ad3db8047038c6504
Tracy Phillips <tracphil@outlook.com>
2022-01-13 10:37:11 -0500

    removed zsh setup and added fish alias/function

---
+++ config/.config/fish/functions/monk.fish
@@ -0,0 +1,3 @@
+function monk --wraps='cd ~/Projects/monkey/git-repos' --description 'alias monk cd ~/Projects/monkey/git-repos'
+  cd ~/Projects/monkey/git-repos $argv; 
+end
--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if ! command -v antibody &> /dev/null
-then
-    echo "antibody is required for this zsh configuration"
-    exit
-fi
+#if ! command -v antibody &> /dev/null
+# then
+#    echo "antibody is required for this zsh configuration"
+#    exit
+#fi
 
 # Put .stowrc in place first
 stow -R -t $HOME stow
@@ -12,6 +12,6 @@ stow -R -t $HOME stow
 # Now stow all directories
 stow -R -t $HOME */
 
-cd zsh
-antibody bundle < antibody_plugins.txt > ~/.antibody_plugins.sh
-git clone https://github.com/mantoso/pyenv-default-requirements.git $(pyenv root)/plugins/pyenv-default-requirements
+#cd zsh
+#antibody bundle < antibody_plugins.txt > ~/.antibody_plugins.sh
+#git clone https://github.com/mantoso/pyenv-default-requirements.git $(pyenv root)/plugins/pyenv-default-requirements
